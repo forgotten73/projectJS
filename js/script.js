1e38fe8de07eb2a548c5eaaa5bda768a7b240873
forgotten73 <chester14011992@gmail.com>
2021-06-30 11:24:33 +0400

    циклы и условия

--- js/script.js
+++ js/script.js
@@ -38,14 +38,52 @@ let personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
 //* Записываем в свойство movies ответы от пользователя
-personalMovieDB.movies[a] = b; //? movies.a = b;
-personalMovieDB.movies[c] = d;
+/* personalMovieDB.movies[a] = b; //? movies.a = b;
+personalMovieDB.movies[c] = d; */
+
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null  && a != '' && b != '' && a.length < 50) {
+
+        personalMovieDB.movies[a] = b; //? movies.a = b; 
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+} */
+
+
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+          i++;
+    if (a != null && b != null  && a != '' && b != '' && a.length < 50) {
+
+        personalMovieDB.movies[a] = b; //? movies.a = b; 
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ты киноман, Гарри!');
+} else {
+    console.log("Ошибка!");
+}
 
 console.log(personalMovieDB);
 
